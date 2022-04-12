a9d2c7f6ddd70f4644a26e0c045b33f13e9a70e1
Rasmus Skovbo <rasmusskovbo.dk@gmail.com>
2022-04-12 23:50:35 +0200

    Implemented and fixed proper speed-based turn management

--- Assets/Scripts/Combat/SpeedManager.cs
+++ Assets/Scripts/Combat/SpeedManager.cs
@@ -1,74 +1,168 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+
+public class TurnUnit : IComparable
+{
+    public TurnUnit(Unit unit, float turnRatio)
+    {
+        this.unit = unit;
+        this.turnRatio = turnRatio;
+        turnCounter = turnRatio;
+    }
+
+    private Unit unit;
+    private float turnRatio;
+    private float turnCounter;
+
+    public Unit Unit
+    {
+        get => unit;
+        set => unit = value;
+    }
+
+    public float TurnRatio
+    {
+        get => turnRatio;
+        set => turnRatio = value;
+    }
+
+    public float TurnCounter
+    {
+        get => turnCounter;
+        set => turnCounter = value;
+    }
+    
+    public int CompareTo(object obj)
+    {
+        TurnUnit other = obj as TurnUnit;
+        return this.turnCounter.CompareTo(other.turnCounter);
+        //return Mathf.RoundToInt(this.CurrentSpeed - other.CurrentSpeed);
+    }
+}
 
 public class SpeedManager
 {
+    /*
+     * 1) Get all active units
+     * 2) Find the fastest (sort)
+     * 3) Fastest is the baseline (will first every round)
+     * 4) Use ratio (fastest speed/slower unit speed)
+     * 5) If unit's turn counter < current turn + 1 -> act and increment (unit's turn counter + unit's turn ratio)
+     * 6) Else, do not increment and check again next round.
+     */
     public SpeedManager(List<GameObject> incomingUnits)
     {
-        activeUnits = new List<Unit>();
+        sortedUnits = new List<Unit>();
+        activeUnits = new List<TurnUnit>();
         
-        foreach (GameObject go in incomingUnits)
-        {
-            Unit component = go.GetComponent<Unit>();
-            activeUnits.Add(component);
-        }
+        incomingUnits.ForEach(go => sortedUnits.Add(go.GetComponent<Unit>()));
+        sortedUnits.Sort();
         
-        PrintTurnOrder();
-        Debug.Log(activeUnits.Count);
-        activeUnits.Sort();
-        PrintTurnOrder();
-        
-        SetCounter();
+        SetupTurnList();
     }
 
-    private List<Unit> activeUnits;
+    private List<Unit> sortedUnits;
+    
+    private List<TurnUnit> activeUnits;
     private Unit nextUnitToAct;
-    private float counter;
+    
+    private float fastestSpeed;
+    private int currentTurn = 1;
 
-    public void SetCounter()
+    public void SetupTurnList()
     {
-        counter = 0;
-        
-        foreach (Unit unit in activeUnits)
+        fastestSpeed = sortedUnits[0].CurrentSpeed;
+
+        sortedUnits.ForEach(unit =>
         {
-            if (unit.CurrentSpeed > counter)
-            {
-                counter = unit.CurrentSpeed;
-            }
-        }
+            activeUnits.Add(GetTurnUnit(unit));
+        });
+        
+        activeUnits.Sort();
     }
 
+    public TurnUnit GetTurnUnit(Unit unit)
+    {
+        float turnRatio = fastestSpeed / unit.CurrentSpeed;
+
+        return new TurnUnit(
+            unit,
+            turnRatio
+        );
+    }
+    
+    public Unit GetNextTurn()
+    {
+        List<TurnUnit> unitsToActThisTurn = new List<TurnUnit>();
+        
+        // Check units turn counter for turn
+        activeUnits.ForEach(unit =>
+        {
+            if (unit.TurnCounter < currentTurn + 1)
+            {
+                unitsToActThisTurn.Add(unit);
+            }
+
+        });
+
+        // If no units are left to act this turn then increase turn counter
+        // and call function again to return first from next turn
+        if (unitsToActThisTurn.Count > 0)
+        {
+            // Make sure the lowest unit with turn counter starts this turn
+            unitsToActThisTurn.Sort();
+        
+            // Cache reference
+            TurnUnit nextUnitToActThisTurn = unitsToActThisTurn[0];
+        
+            // Increase the turn counter of the unit that's about to act
+            activeUnits.Find(turnUnit => turnUnit.Equals(nextUnitToActThisTurn)).TurnCounter +=
+                nextUnitToActThisTurn.TurnRatio;  
+        
+            // Return the unit to combat system.
+        
+            return unitsToActThisTurn[0].Unit;
+        }
+        else
+        {
+            currentTurn++;
+            return GetNextTurn();
+        }
+        
+    }
+
+    /*
     public Unit GetNextTurn()
     {
         // Need to update active units after each turn
         // In case a unit died, remove it from list.
         // Seems like it doesnt add to the back of the list. Investigate
-        foreach (Unit unit in activeUnits)
+        foreach (Unit unit in sortedUnits)
         {
-            Debug.Log("Counter: " + counter);
+            Debug.Log("Counter: " + fastestSpeed);
             Debug.Log("Unit speed: " + unit.CurrentSpeed);
-            if (unit.CurrentSpeed <= counter)
+            if (unit.CurrentSpeed <= fastestSpeed)
             {
                 nextUnitToAct = unit;
                 
-                activeUnits.Remove(nextUnitToAct);
-                counter--;
+                sortedUnits.Remove(nextUnitToAct);
+                fastestSpeed--;
                 Debug.Log("Next unit to act: " + nextUnitToAct.UnitName);
                 break;
             }
-            counter--;
-            if (counter <= 0) SetCounter();
+            fastestSpeed--;
+            if (fastestSpeed <= 0) SetupTurnList();
         }
         
-        activeUnits.Add(nextUnitToAct);
+        sortedUnits.Add(nextUnitToAct);
         return nextUnitToAct;
     }
+    */
 
     public void PrintTurnOrder()
     {
-        foreach (Unit unit in activeUnits)
+        foreach (Unit unit in sortedUnits)
         {
             Debug.Log("Unit order: " + unit.UnitName + ". Speed: " + unit.CurrentSpeed);
         }
